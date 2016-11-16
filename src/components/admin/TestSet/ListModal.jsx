949a5e54ad0566b28842dc8dd0d752df8e57b89d
iboying <weboying@gmail.com>
2016-11-16 17:03:50 +0800

    complete admin activities

--- src/components/admin/TestSet/ListModal.jsx
+++ src/components/admin/TestSet/ListModal.jsx
@@ -3,92 +3,113 @@ import React, { Component, PropTypes } from 'react';
 import SuperAgent from 'superagent';
 import { Row, Col, Modal, Form, Button, Input, Icon, DatePicker, Select } from 'antd';
 import styles from './ListModal.less';
+import moment from 'moment';
 
+const { string, number, shape, arrayOf, func } = PropTypes;
 const FormItem = Form.Item;
 const RangePicker = DatePicker.RangePicker;
 const Option = Select.Option;
 
 class ListModal extends Component {
-  static defaultProps = {
-      onChange: new Function
+  state = {
+    loading: false
   }
 
-  static PropTypes = {
-      onChange: React.PropTypes.func
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      visible: false,
-    };
-  }
-
+  /**
+   * 修改时间段
+   */
   onChange(dates, dateStrings) {
     console.log('From: ', dates[0], ', to: ', dates[1]);
     console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
   }
 
+  /**
+   * 考核年度改变
+   */
   handleChange(value) {
     console.log(`selected ${value}`);
   }
 
   getYearString(){
-    var options = []
-    var date = new Date()
-    var year = date.getFullYear()-1
-    for (var i=0; i<=20;i++) {
-      var val = year+i
+    let options = [];
+    let date = new Date();
+    let year = date.getFullYear() - 1;
+    for (let i=0; i<=20;i++) {
+      let val = year+i
       options.push(<Option key={val} value={`${val}`}>{val}</Option>)
     }
-    return options
-  }
-
-  handleOk(e) {
-    this.setState({ loading: true });
-
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-
-    setTimeout(() => {
-      this.setState({ loading: false, visible: false });
-    }, 1000);
-
-    var visible = this.state.visible
-    this.setState({visible}, ()=> this.props.onChange(visible));
+    return options;
   }
 
   handleCancel() {
-    var visible = this.state.visible
-    this.setState({visible}, ()=> this.props.onChange(visible));
+    this.props.form.resetFields();
+    this.props.hideModal();
   }
 
-  // 创建考核
-  pushAppoint(){
-    
+  /**
+   * 提交表单
+   */
+  handleOk(e) {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.setState({ loading: true });
+        console.log('Received values of form: ', values);
+        this.processActivity(values);
+      }
+    });
   }
 
-  //更新考核
-  putAppoint(){
-     
+  /**
+   * 更新或创建考核活动
+   */
+  processActivity(fromData) {
+    let {time_line_year, first_range, second_range, third_range} = fromData;
+    let method = this.props.activity.id ? 'put' : 'post';
+    let id = this.props.activity.id ? this.props.activity.id : '';
+    let format = "YYYY-MM-DD";
+
+    SuperAgent(method, `http://114.55.172.35:3232/admin/activities/${id}`)
+      .set('Accept', 'application/json')
+      .set('X-Admin-Token', sessionStorage.admin_token)
+      .set('X-Admin-Email', sessionStorage.admin_email)
+      .send({
+        activity: {
+          activity_created_year: time_line_year,
+          first_phase_begin: moment(first_range[0], format),
+          first_phase_end: moment(first_range[1], format),
+          second_phase_begin: moment(second_range[0], format),
+          second_phase_end: moment(second_range[1], format),
+          third_phase_begin: moment(third_range[0], format),
+          third_phase_end: moment(third_range[1], format)
+        }
+      })
+      .end( (err, res) => {
+        if (!err || err === null) {
+          console.log("创建或更新考核活动成功");
+          console.dir(res.body);
+          this.props.hideModal();
+          this.props.refreshData();
+          this.setState({ loading: false });
+        } else {
+          console.log("创建或更新考核活动失败");
+        }
+      })
   }
 
   render() {
-    const title = []
+    const title = [];
     title.push(
-      this.props.record.id?<div key={1} className={styles.modal_title}>编辑考核</div>:<div key={1} className={styles.modal_title}>新建考核</div>
-    )
+      this.props.activity.id ? 
+        <div key={1} className={styles.modal_title}>编辑考核</div> : 
+        <div key={1} className={styles.modal_title}>新建考核</div>
+    );
 
-    const options = []
-    var date = new Date()
-    var year = date.getFullYear()-5
-    for (var i=0; i<=20;i++) {
-      var val = year+i
+    let options = [];
+    let date = new Date();
+    let _year = date.getFullYear() - 5;
+    for (let i=0; i<=20;i++) {
+      let val = _year + i;
       options.push(<Option key={val} value={`${val}`}>{val}</Option>)
     }
 
@@ -97,55 +118,100 @@ class ListModal extends Component {
       labelCol: { span: 6 },
       wrapperCol: { span: 16 },
     }
+    
+    // 表单默认值
+    let {id, year, first_begin, first_end, second_begin, second_end, third_begin, third_end} = this.props.activity;
+    let first_range = '';
+    let second_range = '';
+    let third_range = '';
+    let year_value = '';
+
+    if (id) {
+      year_value = year+"";
+      first_range = [moment(first_begin, "YYYY-MM-DD"), moment(first_end, "YYYY-MM-DD")];
+      second_range = [moment(second_begin, "YYYY-MM-DD"), moment(second_end, "YYYY-MM-DD")];
+      third_range = [moment(third_begin, "YYYY-MM-DD"), moment(third_end, "YYYY-MM-DD")];
+    } 
+
     return (
       <div className={styles.link_btn}>
         <Modal
             width="36vw"
             style={{ top: 100 }}
             visible={this.props.visible}
-            title={title}
-            onOk={this.handleOk.bind(this)}
             onCancel={this.handleCancel.bind(this)}
-            footer={[]}
-          >
-          <Form horizontal onSubmit={this.handleOk.bind(this)}>
-            <Row className={styles.modal_content}>
-              <FormItem className={styles.form_item} labelCol={{ span: 5 }} help label="考核年度" {...formItemLayout}>
-                {getFieldDecorator("time_line_year")(
-                  <Select style={{ width: 200 }} placeholder="请选择年份" 
-                    onChange={this.handleChange}
-                    notFoundContent=""
-                  >
-                    {options}
-                  </Select>
-                )}
-              </FormItem>
-              <FormItem label="上传登记表时间" labelCol={{ span: 5 }} help className={styles.form_item} {...formItemLayout}>
-                {getFieldDecorator("time_line_one")(
-                  <RangePicker format="MM.DD" onChange={this.onChange} />
-                )}
-              </FormItem>
-              <FormItem label="在线考核时间" labelCol={{ span: 5 }} help className={styles.form_item} {...formItemLayout}>
-                {getFieldDecorator("time_line_two")(
-                  <RangePicker format="MM.DD" onChange={this.onChange} />
-                )}
-              </FormItem>
-              <FormItem label="考核统计时间" labelCol={{ span: 5 }} help className={styles.form_item} {...formItemLayout}>
-                {getFieldDecorator("time_line_three")(
-                  <RangePicker format="MM.DD" onChange={this.onChange} />
-                )}
-              </FormItem>
-              <FormItem className={styles.form_submit}>
-                <Button type="primary" htmlType="submit" size="large" loading={this.state.loading} >
-                  提交
-                </Button>
-              </FormItem>
-            </Row>
-          </Form>
+            title={title}
+            footer={false}>
+            <Form horizontal onSubmit={this.handleOk.bind(this)}>
+              <Row className={styles.modal_content}>
+                <FormItem className={styles.form_item} labelCol={{ span: 5 }} help label="考核年度" {...formItemLayout}>
+                  {getFieldDecorator("time_line_year", {
+                    initialValue: year_value,
+                    rules: [{ type: 'string', required: true, message: '请选择考核年度' }]
+                  })(
+                    <Select style={{ width: 200 }} 
+                      placeholder="请选择年份" 
+                      onChange={this.handleChange}
+                      notFoundContent="请选择考核年度">
+                      {options}
+                    </Select>
+                  )}
+                </FormItem>
+                <FormItem label="上传登记表时间" labelCol={{ span: 5 }} help className={styles.form_item} {...formItemLayout}>
+                  {getFieldDecorator("first_range", {
+                    initialValue: first_range,
+                    rules: [{ type: 'array', required: true, message: '请选择上传登记表时间' }]
+                  })(
+                    <RangePicker format="YYYY-MM-DD" onChange={this.onChange} />
+                  )}
+                </FormItem>
+                <FormItem label="在线考核时间" labelCol={{ span: 5 }} help className={styles.form_item} {...formItemLayout}>
+                  {getFieldDecorator("second_range", {
+                    initialValue: second_range,
+                    rules: [{ type: 'array', required: true, message: '请选择上传登记表时间' }]
+                  })(
+                    <RangePicker format="YYYY-MM-DD" onChange={this.onChange} />
+                  )}
+                </FormItem>
+                <FormItem label="考核统计时间" labelCol={{ span: 5 }} help className={styles.form_item} {...formItemLayout}>
+                  {getFieldDecorator("third_range", {
+                    initialValue: third_range,
+                    rules: [{ type: 'array', required: true, message: '请选择上传登记表时间' }]
+                  })(
+                    <RangePicker format="YYYY-MM-DD" onChange={this.onChange} />
+                  )}
+                </FormItem>
+                <FormItem className={styles.form_submit}>
+                  <Button type="primary" htmlType="submit" size="large" loading={this.state.loading} >
+                    提交
+                  </Button>
+                </FormItem>
+              </Row>
+            </Form>
         </Modal>
       </div>
     );
   }
+}
+
+ListModal.defaultProps = {
+  activity: {},
+  hideModal: new Function,
+  refreshData: new Function
+}
+
+ListModal.propTypes = {
+  activity: shape({
+    id: number,
+    first_begin: string, 
+    first_end: string,
+    second_begin: string,
+    second_end: string,
+    third_begin: string,
+    third_end: string
+  }),
+  hideModal: func,
+  refreshData: func
 }
 
 ListModal = Form.create({})(ListModal);
--- src/components/admin/TestSet/TestSet.jsx
+++ src/components/admin/TestSet/TestSet.jsx
@@ -7,126 +7,184 @@ import Admin from '../Admin';
 import classnames from 'classnames'
 import { Link } from 'react-router'
 import { Icon, Button, Table, Modal } from 'antd'
-
-function onChange(pagination, filters, sorter) {
-  console.log('params', pagination, filters, sorter);
-}
+import moment from 'moment';
 
 const confirm = Modal.confirm;
-
-const data = [{
-  point1: 2016,
-  point2: "07.01--07.31",
-  point3: "08.01--08.31",
-  point4: "09.01--09.30",
-}, {
-  point1: 2015,
-  point2: "07.01--07.31",
-  point3: "08.01--08.31",
-  point4: "09.01--09.30",
-}, {
-  point1: 2014,
-  point2: "07.01--07.31",
-  point3: "08.01--08.31",
-  point4: "09.01--09.30",
-}, {
-  point1: 2013,
-  point2: "07.01--07.31",
-  point3: "08.01--08.31",
-  point4: "09.01--09.30",
-}];
+const ButtonGroup = Button.Group;
 
 class TestSet extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-      record: {},
-    }
+  state = {
+    visible: false,
+    record: {},
+    activities: []
   }
 
   componentWillMount() {
-    // this.getData()
+    this.getActivities();
   }
 
+  getActivities(){
+    SuperAgent
+      .get("http://114.55.172.35:3232/admin/activities?page=1&per_page=1000")
+      .set('Accept', 'application/json')
+      .set('X-Admin-Token', sessionStorage.admin_token)
+      .set('X-Admin-Email', sessionStorage.admin_email)
+      .end( (err, res) => {
+        if (!err || err === null) {
+          console.log("获取所有考核活动成功");
+          console.dir(res.body);
+          this.setState({ activities: this.setDataSource(res.body.activities)});
+        } else {
+          console.log("获取所有考核活动失败");
+        }
+      })
+  }
+
+  // 整理数据源
+  setDataSource(activities) {
+    let array = [];
+    for(let activity of activities) {
+      let first_begin = moment(activity.first_phase_begin).format('YYYY-MM-DD');
+      let first_end = moment(activity.first_phase_end).format('YYYY-MM-DD');
+      let second_begin = moment(activity.second_phase_begin).format('YYYY-MM-DD');
+      let second_end = moment(activity.second_phase_end).format('YYYY-MM-DD');
+      let third_begin = moment(activity.third_phase_begin).format('YYYY-MM-DD');
+      let third_end = moment(activity.third_phase_end).format('YYYY-MM-DD');
+
+      let _cache = {
+        id: activity.id,
+        year: activity.activity_created_year,
+        first_begin: first_begin,
+        first_end: first_end,
+        second_begin: second_begin,
+        second_end: second_end,
+        third_begin: third_begin,
+        third_end: third_end,
+        column_first: `${first_begin} ~ ${first_end}`,
+        column_second: `${second_begin} ~ ${second_end}`,
+        column_third: `${third_begin} ~ ${third_end}`
+      };
+      array.push(_cache);
+    }
+    console.log("整理的数据源");
+    console.dir(array);
+    return array;
+  }
+
+  /**
+   * 表格改变事件
+   */
+  onChange(pagination, filters, sorter) {
+    console.log('params', pagination, filters, sorter);
+  }
+
+  /**
+   * 表格条目点击
+   */
   detail_cell(record, index){
-    // this.props.router.replace(`/statistics_d?id=${index}`)
+    
   }
 
-  showModal(record) {
-    console.log('show modal');
-    this.setState({
-      visible: true,
-      record: record
+  /**
+   * 删除确认
+   */
+  deleteConfirm(that, activity) {
+    confirm({
+      title: '确定删除该考核活动吗?',
+      content: '删除后不可以恢复，请谨慎操作。',
+      onOk() {
+        console.log("开始删除");
+        that.deleteActivity(activity);
+      },
+      onCancel() {
+        console.log("取消删除");
+      },
     });
+  }
+
+  /**
+   * 调用接口，删除考核
+   */
+  deleteActivity(activity) {
+    console.dir(activity);
   }
 
   getColumns(){
     const columns = [{
       title: '考核年度',
-      dataIndex: 'point1',
-      sorter: (a, b) => a.point1 - b.point1,
+      dataIndex: 'year',
+      sorter: (a, b) => a.year - b.year,
     }, {
       title: '上传登记表时间',
-      dataIndex: 'point2',
-      sorter: (a, b) => a.point2 - b.point2,
+      dataIndex: 'column_first',
     }, {
       title: '在线考核时间',
-      dataIndex: 'point3',
-      sorter: (a, b) => a.point3 - b.point3,
+      dataIndex: 'column_second',
     }, {
       title: '分数统计时间',
-      dataIndex: 'point4',
-      sorter: (a, b) => a.point4 - b.point4,
+      dataIndex: 'column_third',
     },{
       title: '操作',
       dataIndex: '',
       key: 'x',
       render: (text, record, index) => this.change_icon(record)
     }];
-    return columns
+    return columns;
   }
 
+  /**
+   * 自定义操作列
+   */
   change_icon(record){
     const change_icon = []
     change_icon.push(
       <div key='icon' className={css.icon_content}>
-        <Link onClick={this.showModal.bind(this, record)}>详情</Link>
+        <ButtonGroup>
+          <Button type="primary" icon="edit" onClick={this.showModal.bind(this, record)}/>
+          <Button type="primary" icon="delete" onClick={this.deleteConfirm.bind(this, this, record)} />
+        </ButtonGroup>
       </div>
     )
     return change_icon
   }
 
-  deleteList(val){
-    console.log(val);
-    this.setState({visible: val})
-    // this.getData()
+  /**
+   * 新建、编辑模态框
+   */
+  showModal(record) {
+    console.log(record);
+    this.setState({
+      visible: true,
+      record: record
+    });
   }
 
-
-  getData(){
-    var token = localStorage.token
-    var phone = localStorage.phone
-    var url = "http://114.55.172.35:5555/admin/orders/checked?page=1&per_page=10000"
-    SuperAgent.get(url)
-              .set('Accept', 'application/json')
-              .set('X-Administrator-Token', token)
-              .set('X-Administrator-Phone', phone)
-              .end( (err, res) => {
-                if (res.ok) {
-                  var data = res.body.checked_orders
-                  this.setState({order_data: data})
-                }
-              })
+  handleModalVisible(){
+    console.log("清楚了");
+    this.setState({
+      visible: false
+    });
   }
 
   render() {
+    const { activities } = this.state;
+
     return (
       <Admin>
         <div className={css.table_content}>
-          <div><Button onClick={this.showModal.bind(this)} type="primary" icon="plus">新建考核</Button></div>
-          <Table columns={this.getColumns()} bordered dataSource={data} onRowClick={this.detail_cell.bind(this)} pagination={{ pageSize: 9 }} onChange={onChange} />
-          <ListModal {...this.state} onChange={val => this.deleteList(val)} />
+          <Button onClick={this.showModal.bind(this, {})} type="primary" icon="plus">新建考核</Button>
+
+          <Table columns={this.getColumns()} 
+                 bordered 
+                 dataSource={activities} 
+                 onRowClick={this.detail_cell.bind(this)} 
+                 pagination={false} 
+                 onChange={this.onChange} />
+
+          <ListModal visible={this.state.visible} 
+                     activity={this.state.record}
+                     hideModal={this.handleModalVisible.bind(this)}
+                     refreshData={this.getActivities.bind(this)} />
         </div>
       </Admin>
     )
